bec34c6ef920bc1eb9fcf6cf320f8c5bb1d8adc2
neoko <33611354+neokofg@users.noreply.github.com>
2023-03-23 02:35:18 +0900

    Update BotController.php

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -13,100 +13,104 @@ class BotController extends Controller
     // 6112927855:AAF-Rc36LyNcLeFuyjJw8vdEfDBw_QEnhMo
     protected function botResponse()
     {
-        $result = file_get_contents('php://input');
-        $update = json_decode($result);
+        try {
+            $result = file_get_contents('php://input');
+            $update = json_decode($result);
 
-        if (isset($update->message->text)) {
-            $user = Telegram::where('user_id', '=', $update->message->from->id)->first();
-            if ($update->message->text == '/start') {
-                if ($user == null) {
+            if (isset($update->message->text)) {
+                $user = Telegram::where('user_id', '=', $update->message->from->id)->first();
+                if ($update->message->text == '/start') {
+                    if ($user == null) {
+                        $userdata = array(
+                            'user_id' => $update->message->from->id,
+                            'name' => '@' . $update->message->from->username,
+                            'status' => 'started',
+                        );
+                        Telegram::create($userdata);
+
+                        $data = [
+                            'chat_id' => $update->message->chat->id,
+                            'reply_to_message_id' => $update->message->message_id,
+                            'text' => 'Здравствуйте! Я ваш личный помощник по агрегатору Tourclick!!!'
+                        ];
+                        $response = Http::get("https://api.telegram.org/bot6112927855:AAF-Rc36LyNcLeFuyjJw8vdEfDBw_QEnhMo/sendMessage?" . http_build_query($data));
+                        $data = [
+                            'chat_id' => $update->message->chat->id,
+                            'text' => 'Для начала войдите в свой аккаунт'
+                        ];
+                        $response = Http::get("https://api.telegram.org/bot6112927855:AAF-Rc36LyNcLeFuyjJw8vdEfDBw_QEnhMo/sendMessage?" . http_build_query($data));
+                        $data = [
+                            'chat_id' => $update->message->chat->id,
+                            'text' => 'Введите свой email'
+                        ];
+                        $response = Http::get("https://api.telegram.org/bot6112927855:AAF-Rc36LyNcLeFuyjJw8vdEfDBw_QEnhMo/sendMessage?" . http_build_query($data));
+                    }else{
+                        $data = [
+                            'chat_id' => $update->message->chat->id,
+                            'reply_to_message_id' => $update->message->message_id,
+                            'text' => 'Для начала войдите в свой аккаунт'
+                        ];
+                        $response = Http::get("https://api.telegram.org/bot6112927855:AAF-Rc36LyNcLeFuyjJw8vdEfDBw_QEnhMo/sendMessage?" . http_build_query($data));
+                        $data = [
+                            'chat_id' => $update->message->chat->id,
+                            'text' => 'Введите свой email'
+                        ];
+                        $response = Http::get("https://api.telegram.org/bot6112927855:AAF-Rc36LyNcLeFuyjJw8vdEfDBw_QEnhMo/sendMessage?" . http_build_query($data));
+                    }
+                } else if ($user->status == 'started') {
                     $userdata = array(
-                        'user_id' => $update->message->from->id,
-                        'name' => '@' . $update->message->from->username,
-                        'status' => 'started',
-                    );
-                    Telegram::create($userdata);
-
-                    $data = [
-                        'chat_id' => $update->message->chat->id,
-                        'reply_to_message_id' => $update->message->message_id,
-                        'text' => 'Здравствуйте! Я ваш личный помощник по агрегатору Tourclick!!!'
-                    ];
-                    $response = Http::get("https://api.telegram.org/bot6112927855:AAF-Rc36LyNcLeFuyjJw8vdEfDBw_QEnhMo/sendMessage?" . http_build_query($data));
-                    $data = [
-                        'chat_id' => $update->message->chat->id,
-                        'text' => 'Для начала войдите в свой аккаунт'
-                    ];
-                    $response = Http::get("https://api.telegram.org/bot6112927855:AAF-Rc36LyNcLeFuyjJw8vdEfDBw_QEnhMo/sendMessage?" . http_build_query($data));
-                    $data = [
-                        'chat_id' => $update->message->chat->id,
-                        'text' => 'Введите свой email'
-                    ];
-                    $response = Http::get("https://api.telegram.org/bot6112927855:AAF-Rc36LyNcLeFuyjJw8vdEfDBw_QEnhMo/sendMessage?" . http_build_query($data));
-                }else{
-                    $data = [
-                        'chat_id' => $update->message->chat->id,
-                        'reply_to_message_id' => $update->message->message_id,
-                        'text' => 'Для начала войдите в свой аккаунт'
-                    ];
-                    $response = Http::get("https://api.telegram.org/bot6112927855:AAF-Rc36LyNcLeFuyjJw8vdEfDBw_QEnhMo/sendMessage?" . http_build_query($data));
-                    $data = [
-                        'chat_id' => $update->message->chat->id,
-                        'text' => 'Введите свой email'
-                    ];
-                    $response = Http::get("https://api.telegram.org/bot6112927855:AAF-Rc36LyNcLeFuyjJw8vdEfDBw_QEnhMo/sendMessage?" . http_build_query($data));
-                }
-            } else if ($user->status == 'started') {
-                $userdata = array(
-                    'status' => 'password',
-                    'input' => $update->message->text
-                );
-                Telegram::where('user_id', '=', $update->message->from->id)->update($userdata);
-                $data = [
-                    'chat_id' => $update->message->chat->id,
-                    'text' => 'Введите пароль',
-                    'reply_to_message_id' => $update->message->message_id,
-                ];
-                $response = Http::get("https://api.telegram.org/bot6112927855:AAF-Rc36LyNcLeFuyjJw8vdEfDBw_QEnhMo/sendMessage?" . http_build_query($data));
-            } else if ($user->status == 'password') {
-                $userdata = array(
-                    'status' => 'login'
-                );
-                Telegram::where('user_id', '=', $update->message->from->id)->update($userdata);
-
-                $formFields = array([
-                    'email' => $user->input,
-                    'password' => $update->message->text
-                ]);
-                $auth = Auth::attempt($formFields);
-                if ($auth) {
-                    TourbaseUser::where('user_id',$auth->id)->update([
-                        'botStatus' => 'logined',
-                        'botUser' => $update->message->from->id,
-                    ]);
-                    $data = [
-                        'chat_id' => $update->message->chat->id,
-                        'text' => 'Вы успешно вошли!',
-                    ];
-                    $response = Http::get("https://api.telegram.org/bot6112927855:AAF-Rc36LyNcLeFuyjJw8vdEfDBw_QEnhMo/sendMessage?" . http_build_query($data));
-                    $data = [
-                        'chat_id' => $update->message->chat->id,
-                        'text' => 'Теперь к вам будут приходить PUSH-уведомления в случае брони вашей турбазы!',
-                    ];
-                    $response = Http::get("https://api.telegram.org/bot6112927855:AAF-Rc36LyNcLeFuyjJw8vdEfDBw_QEnhMo/sendMessage?" . http_build_query($data));
-                } else {
-                    $userdata = array(
-                        'status' => 'started',
+                        'status' => 'password',
                         'input' => $update->message->text
                     );
                     Telegram::where('user_id', '=', $update->message->from->id)->update($userdata);
                     $data = [
                         'chat_id' => $update->message->chat->id,
-                        'text' => 'Неправильный логин или пароль!',
+                        'text' => 'Введите пароль',
+                        'reply_to_message_id' => $update->message->message_id,
                     ];
                     $response = Http::get("https://api.telegram.org/bot6112927855:AAF-Rc36LyNcLeFuyjJw8vdEfDBw_QEnhMo/sendMessage?" . http_build_query($data));
+                } else if ($user->status == 'password') {
+                    $userdata = array(
+                        'status' => 'login'
+                    );
+                    Telegram::where('user_id', '=', $update->message->from->id)->update($userdata);
+
+                    $formFields = array([
+                        'email' => $user->input,
+                        'password' => $update->message->text
+                    ]);
+                    $auth = Auth::attempt($formFields);
+                    if ($auth) {
+                        TourbaseUser::where('user_id',$auth->id)->update([
+                            'botStatus' => 'logined',
+                            'botUser' => $update->message->from->id,
+                        ]);
+                        $data = [
+                            'chat_id' => $update->message->chat->id,
+                            'text' => 'Вы успешно вошли!',
+                        ];
+                        $response = Http::get("https://api.telegram.org/bot6112927855:AAF-Rc36LyNcLeFuyjJw8vdEfDBw_QEnhMo/sendMessage?" . http_build_query($data));
+                        $data = [
+                            'chat_id' => $update->message->chat->id,
+                            'text' => 'Теперь к вам будут приходить PUSH-уведомления в случае брони вашей турбазы!',
+                        ];
+                        $response = Http::get("https://api.telegram.org/bot6112927855:AAF-Rc36LyNcLeFuyjJw8vdEfDBw_QEnhMo/sendMessage?" . http_build_query($data));
+                    } else {
+                        $userdata = array(
+                            'status' => 'started',
+                            'input' => $update->message->text
+                        );
+                        Telegram::where('user_id', '=', $update->message->from->id)->update($userdata);
+                        $data = [
+                            'chat_id' => $update->message->chat->id,
+                            'text' => 'Неправильный логин или пароль!',
+                        ];
+                        $response = Http::get("https://api.telegram.org/bot6112927855:AAF-Rc36LyNcLeFuyjJw8vdEfDBw_QEnhMo/sendMessage?" . http_build_query($data));
+                    }
                 }
             }
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 }
