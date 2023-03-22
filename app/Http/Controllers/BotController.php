23d28a08da28aec39dcd90c952effbef14e8ab9d
neoko <33611354+neokofg@users.noreply.github.com>
2023-03-23 02:41:08 +0900

    Update BotController.php

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Telegram;
 use App\Models\TourbaseUser;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
 
