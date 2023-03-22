00da9aa72e9fa97a5ef7c5818bc777464f35a493
neoko <33611354+neokofg@users.noreply.github.com>
2023-03-23 06:16:12 +0900

    Update TourBaseController.php

--- app/Http/Controllers/TourBaseController.php
+++ app/Http/Controllers/TourBaseController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Validator;
 
 class TourBaseController extends Controller
 {
